c6f344089538135c17f783bdae25e229d7bef910
Vincent Driessen <vincent@datafox.nl>
2010-08-09 12:50:17 +0200

    Remove unused function.

--- lib/default.rb
+++ lib/default.rb
@@ -3,12 +3,6 @@
 
 # Extend Item
 class Nanoc3::Item
-  def content(opts = {})
-    opts[:rep] ||= :default
-    opts[:snapshot] ||= :last
-    reps.find { |r| r.name == opts[:rep] }.content_at_snapshot(opts[:snapshot])
-  end
-
   def name
     identifier.split("/").last
   end
