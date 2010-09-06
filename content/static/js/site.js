ae2c1e76835ea90634cc3b0d928e392f715857a0
Vincent Driessen <vincent@datafox.nl>
2010-09-07 01:08:37 +0200

    Make the RSS icon smaller.

--- content/static/js/site.js
+++ content/static/js/site.js
@@ -1,5 +1,14 @@
+/* 
+ * Put it all inside of a function, so global variables become scoped, which
+ * means the Clojure Compiler can obfuscate these, too!
+ */
+
+function nviecom() {
+
 var MAIN_WIDTH = 600;
 var RESTORE_WIDTH = 520;
+var RSS_LOGO_LEFT = 905;
+var RSS_LOGO_WIDTH = 32;
 
 $(document).ready(function () {
 	$('.reldate').relatizeDate();
@@ -28,10 +37,11 @@ $(document).ready(function () {
 	);
 
 	function showhide_rss(id, show) {
+		var pixels_string = (RSS_LOGO_WIDTH / 2) + "px";
 		$(id).stop(true, true)
-			.css({'left': 889, 'z-index': (show) ? 1 : 3})
-			.animate({ 'left': '+=30px' }, 150, function() { $(id).css({'z-index': (show) ? 3 : 1 }); })
-			.animate({ 'left': '-=30px' }, 150);
+			.css({'left': RSS_LOGO_LEFT, 'z-index': (show) ? 1 : 3})
+			.animate({ 'left': '+=' + pixels_string }, 150, function() { $(id).css({'z-index': (show) ? 3 : 1 }); })
+			.animate({ 'left': '-=' + pixels_string }, 150);
 	}
 
 	var func_timer = null;
@@ -67,3 +77,10 @@ $(window).load(function () {
 		$(this).css({'width': sum, 'left': l});
 	});
 });
+
+}
+
+/*
+ * Load the whole body of the JavaScript
+ */
+nviecom();
