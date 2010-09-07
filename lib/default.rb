c3c3e00dbf2a33f691f1315493cb598717d781e7
Vincent Driessen <vincent@datafox.nl>
2010-09-07 12:04:32 +0200

    Give each post a HTML summary, and use that for index and post overview.

--- lib/default.rb
+++ lib/default.rb
@@ -11,6 +11,25 @@ class Nanoc3::Item
 	parts = self.name.split('-', 4)
 	parts[3].downcase
   end
+
+  def html_summary
+	img_html = ""
+	if self[:image] then
+	  img_html = "<img class=\"postimg\" src=\"/img/postimgs/#{self[:image]}\" alt=\"#{self[:title]}\" />"
+	  img_html = link_to(img_html, self)
+	end
+
+	title_link = link_to(self[:title], self)
+	post_status = ""
+	post_status = " (#{self[:status]})" unless self[:status] == "published"
+	post_date = self[:created_at].strftime('%B %d, %Y')
+	html = "<div class=\"post-summary\">#{img_html}" +
+		   "<h2 style=\"clear: none\">#{title_link}#{post_status}</h2>" +
+	       "<p class=\"postdate\"><span class=\"reldate\">#{post_date}</span></p>" +
+	       "#{self[:excerpt]}" +
+	       "</div>"
+	html
+  end
 end
 
 # Extend Item
