4b9e73d7178ad673131787947d386405e5340c24
Vincent Driessen <vincent@datafox.nl>
2010-09-07 10:30:10 +0200

    Split the JavaScript file into multiple functions.
    They are optimized away by the JS compressor.

--- content/static/js/site.js
+++ content/static/js/site.js
@@ -3,16 +3,18 @@
  * means the Clojure Compiler can obfuscate these, too!
  */
 
-function nviecom() {
+(function () {
 
 var MAIN_WIDTH = 600;
 var RESTORE_WIDTH = 520;
 var RSS_LOGO_LEFT = 905;
 var RSS_LOGO_WIDTH = 32;
 
-$(document).ready(function () {
+function relatize_dates() {
 	$('.reldate').relatizeDate();
+}
 
+function expand_pre_boxes() {
 	// Have them expand when necessary
 	$('pre').hover(
 		function(event) {
@@ -22,7 +24,9 @@ $(document).ready(function () {
 			$('code', this).stop(true, false).delay(500).animate({'max-width': RESTORE_WIDTH + "px"}, 'fast');
 		}
 	);
+}
 
+function open_external_links_in_new_window() {
 	$('a').each(function (index) {
 		var external = $(this).attr('href').indexOf('/') != 0;
 		//alert(index + ": " + $(this).attr('href') + ' => ' + external);
@@ -30,20 +34,24 @@ $(document).ready(function () {
 			$(this).attr('target', '_blank');
 		}
 	});
+}
 
+function hover_browser_icons() {
 	$('#footer img').hover(
 		function () { $(this).attr('src', '/img/' + $(this).attr('id').substr(1) + '1.png'); },
 		function () { $(this).attr('src', '/img/' + $(this).attr('id').substr(1) + '0.png'); }
 	);
+}
 
-	function showhide_rss(id, show) {
-		var pixels_string = (RSS_LOGO_WIDTH / 2) + "px";
-		$(id).stop(true, true)
-			.css({'left': RSS_LOGO_LEFT, 'z-index': (show) ? 1 : 3})
-			.animate({ 'left': '+=' + pixels_string }, 150, function() { $(id).css({'z-index': (show) ? 3 : 1 }); })
-			.animate({ 'left': '-=' + pixels_string }, 150);
-	}
+function showhide_rss(id, show) {
+   	var pixels_string = (RSS_LOGO_WIDTH / 2) + "px";
+   	$(id).stop(true, true)
+   		.css({'left': RSS_LOGO_LEFT, 'z-index': (show) ? 1 : 3})
+   		.animate({ 'left': '+=' + pixels_string }, 150, function() { $(id).css({'z-index': (show) ? 3 : 1 }); })
+   		.animate({ 'left': '-=' + pixels_string }, 150);
+}
 
+function hover_rightbar_icons() {
 	var func_timer = null;
 	$('#rss').hover(
 		function () {
@@ -62,10 +70,9 @@ $(document).ready(function () {
 			}, 2000);
 		}
 	);
-});
+}
 
-// Once all image's are loaded
-$(window).load(function () {
+function auto_align_images() {
 	$('p.autoalign').each(function () {
 		var sum = 0;
 		$('img', this).each(function () {
@@ -76,11 +83,20 @@ $(window).load(function () {
 		//console.log('setting width to ' + sum + ', left to ' + l);
 		$(this).css({'width': sum, 'left': l});
 	});
-});
-
 }
 
-/*
- * Load the whole body of the JavaScript
- */
-nviecom();
+// Once the DOM is fully loaded
+$(document).ready(function () {
+	relatize_dates();
+	expand_pre_boxes();
+	open_external_links_in_new_window();
+	hover_browser_icons();
+	hover_rightbar_icons();
+});
+
+// Once all image's are loaded
+$(window).load(function () {
+	auto_align_images();
+});
+
+})();
