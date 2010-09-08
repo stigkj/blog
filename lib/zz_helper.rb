db9d6394c61093522bfa406566031eed2f34d92c
Vincent Driessen <vincent@datafox.nl>
2010-09-08 15:01:03 +0200

    Remove monkey-patch.
    This is now fixed in nanoc.

--- lib/zz_helper.rb
+++ lib/zz_helper.rb
@@ -11,18 +11,3 @@ include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::XMLSitemap
 
 include RandomTextHelper
-
-# ------------------- PATCH -------------------------
-# Monkey-patch Date to contain an gmtime function
-# Should be structurally fixed by
-# http://projects.stoneship.org/trac/nanoc/ticket/122
-# ---------------------------------------------------
-class Date
-	def to_time
-		Time.parse(self.strftime('%Y/%m/%d'))
-	end
-
-	def gmtime
-		self.to_time.gmtime
-	end
-end
