20105fd1d75acbf820f5e97318a05c141fe0d34b
Vincent Driessen <vincent@datafox.nl>
2010-09-13 01:50:17 +0200

    Update pinger.

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-(curl -s 'http://feedburner.google.com/fb/a/pingSubmit?org.apache.struts.taglib.html.TOKEN=24aab68d458a943935b504586da7b3d0&bloglink=http://dev.nvie.com' | grep -q 'Successfully pinged') && echo "Ping OK. Feedburner now has new content." || echo "Ping failed. Feedburner not updated."
+(curl -s 'http://feedburner.google.com/fb/a/pingSubmit?bloglink=http://dev.nvie.com' | grep -q 'Successfully pinged') && echo "Ping OK for dev.nvie.com. Feedburner now has new content." || echo "Ping failed for dev.nvie.com. Feedburner not updated."
+(curl -s 'http://feedburner.google.com/fb/a/pingSubmit?bloglink=http://nvie.com' | grep -q 'Successfully pinged') && echo "Ping OK for nvie.com. Feedburner now has new content." || echo "Ping failed for nvie.com. Feedburner not updated."
