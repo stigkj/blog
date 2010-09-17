5cbf3c11db1d376bc70e222524f47d337ee8a9df
Vincent Driessen <vincent@datafox.nl>
2010-09-18 01:58:51 +0200

    Distinguish between "local" and "dev" settings.

--- settings/dev.rb
+++ settings/dev.rb
@@ -1,14 +1,11 @@
-#$include_drafts = true
+$include_drafts = true
 #$recent_posts_limit = 7
-$use_googles_jquery = false
-$highlight_syntax = false
-$use_minified_js = false
-$use_minified_css = false
+#$use_googles_jquery = false
+#$highlight_syntax = false
+#$use_minified_js = false
+#$use_minified_css = false
 
 $add_google_analytics = false
 #$add_disqus_comments = false
 #$disqus_develop_mode = false
 #$disqus_shortname = 'nvie'
-
-# Set an .htaccess file, preventing login
-$protect_site = true
